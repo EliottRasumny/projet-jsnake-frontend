cada6d899b79cc445cbf5933d863df472d714c80
Alexandre <alexandre.denys@student.vinci.be>
2021-11-25 15:23:12 +0100

    Change HomePage (button)

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -4,7 +4,7 @@
 
 const HomePage = () => { 
   const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = "<h1>Welcome on the homepage of our snake game for 2 players</h1>";
+  pageDiv.innerHTML = `<h1 class="m-5">JSnake</h1><button class="btn btn-outline-success m-5">1Player</button>`;
 };
 
 export default HomePage;
