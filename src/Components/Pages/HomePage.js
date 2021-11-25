62412e0b89f99cb7941cff570b55c7c0c8032e18
Alexandre <alexandre.denys@student.vinci.be>
2021-11-25 14:54:53 +0100

    Change navbar buttons

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -4,7 +4,7 @@
 
 const HomePage = () => { 
   const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = "Deal with the content of your HomePage";
+  pageDiv.innerHTML = "<h1>Welcome on the homepage of our snake game for 2 players</h1>";
 };
 
 export default HomePage;
