180fac88cae394f8411db1b92900d3be72146586
Alexandre <alexandre.denys@student.vinci.be>
2021-11-25 15:39:57 +0100

    Add image and button 1 and 2 player

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -2,9 +2,13 @@
  * Render the HomePage
  */
 
-const HomePage = () => { 
+const HomePage = () => {
   const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = `<h1 class="m-5">JSnake</h1><button class="btn btn-outline-success m-5">1Player</button>`;
+  pageDiv.innerHTML = `<div class="container fixed-">
+    <h1 class="m-5">JSnake</h1>
+    <button id="btn" class="btn p-3 m-5">1Player</button>
+    <button id="btn" class="btn p-3 m-5">2Player</button>
+  </div>`;
 };
 
 export default HomePage;
