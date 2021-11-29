29c7e8bd91878d87f4dcf5914f40f2cb12831c31
p4c4LAQDH5EokyOZvj9S <cyril.bousmar@student.vinci.be>
2021-11-29 14:17:41 +0100

    indent homepage.js

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -8,18 +8,19 @@ import red_snake from "../../img/red_snake.png";
 
 const HomePage = () => {
   const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = `<h1 class="m-5">JSnake</h1>
+  pageDiv.innerHTML = `
+  <h1 class="m-5">JSnake</h1>
   <div class="d-inline-flex p-1 ">
-  <img src="${blue_snake}" class="rounded float-start" alt="Blue Snake" style="width:8vw ;heigth:auto" >
-  <div class="container">
-  <img src="${red_snake}" class="rounded float-start" alt="Red Snake" style="width:8vw ;heigth:auto">
-  <img src="${blue_snake_inverse}" class="rounded float-start"alt="Blue Snake" style="width:8vw ;heigth:auto">
-  </div>
+    <img src="${blue_snake}" class="rounded float-start" alt="Blue Snake" style="width:8vw ;heigth:auto" >
+    <div class="container">
+      <img src="${red_snake}" class="rounded float-start" alt="Red Snake" style="width:8vw ;heigth:auto">
+      <img src="${blue_snake_inverse}" class="rounded float-start"alt="Blue Snake" style="width:8vw ;heigth:auto">
+    </div>
   </div>
   <div class="container ">
-  <button id="btn" class="btn p-3 m-5">1Player</button>
-  <button id="btn" class="btn p-3 m-5">2Player</button>
-</div>`;
+    <button id="btn" class="btn p-3 m-5">1Player</button>
+    <button id="btn" class="btn p-3 m-5">2Player</button>
+  </div>`;
 };
 
 export default HomePage;
