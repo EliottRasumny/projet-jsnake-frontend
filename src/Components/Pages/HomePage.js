ce6dbfc505ab2e2ac149936ab759f413bccf269f
p4c4LAQDH5EokyOZvj9S <cyril.bousmar@student.vinci.be>
2021-11-29 15:37:30 +0100

    maj container

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -13,12 +13,12 @@ const HomePage = () => {
   <h1 class="m-5">JSnake</h1>
   <div class="d-inline-flex p-1 ">
     <img src="${blue_snake}" class="rounded float-start" alt="Blue Snake" style="width:8vw ;heigth:auto" >
-    <div class="container">
+    <div>
       <img src="${red_snake}" class="rounded float-start" alt="Red Snake" style="width:8vw ;heigth:auto">
       <img src="${blue_snake_inverse}" class="rounded float-start"alt="Blue Snake" style="width:8vw ;heigth:auto">
     </div>
   </div>
-  <div class="container ">
+  <div>
     <button id="btnToSingle" class="btn p-3 m-5" data-uri="/single">1Player</button>
     <button id="btnToDual" class="btn p-3 m-5">2Player</button>
   </div>`;
