c53350a07fd344059f78610d380d5f3da296eef8
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2021-12-13 20:01:33 +0100

    LoginPage

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -11,29 +11,71 @@ function LoginPage() {
   // create a login form
   const form = document.createElement("form");
   form.className = "p-5";
-  const username = document.createElement("input");
-  username.type = "text";
-  username.id = "username";
-  username.placeholder = "username";
-  username.required = true;
-  username.className = "form-control mb-3";
-  const password = document.createElement("input");
-  password.type = "password";
-  password.id = "password";
-  password.required = true;
-  password.placeholder = "password";
-  password.className = "form-control mb-3";
-  const submit = document.createElement("input");
-  submit.value = "Login";
-  submit.type = "submit";
-  submit.className = "btn";
-  submit.id = "btn";
+    const username = document.createElement("input");
+    username.type = "text";
+    username.id = "username";
+    username.placeholder = "username";
+    username.required = true;
+    username.className = "form-control mb-3";
+    const password = document.createElement("input");
+    password.type = "password";
+    password.id = "password";
+    password.required = true;
+    password.placeholder = "password";
+    password.className = "form-control mb-3";
+    const submit = document.createElement("input");
+    submit.value = "Login";
+    submit.type = "submit";
+    submit.className = "btn";
+    submit.id = "btn";
+  form.addEventListener("submit", onSubmit);
   form.appendChild(username);
   form.appendChild(password);
   form.appendChild(submit);
   pageDiv.appendChild(form);
 
  
+  async function onSubmit(e) {
+    e.preventDefault();
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    console.log("In the form : ", username.value, password.value);
+    try {
+      const options = {
+        method: "POST", 
+        body: JSON.stringify({
+          username: username.value,
+          password: password.value,
+        }), // body data type must match "Content-Type" header
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const response = await fetch("/api/auths/login", options); // fetch return a promise => we wait for the response
+      /*
+      if (!response.ok) {
+        throw new Error(
+          "fetch error : " + response.status + " : " + response.statusText
+        );
+      }
+      const user = await response.json(); // json() returns a promise => we wait for the data
+      console.log("user authenticated", user);
+      // save the user into the localStorage
+      setSessionObject("user", user);
+
+      // Rerender the navbar for an authenticated user : temporary step prior to deal with token
+      Navbar({ isAuthenticated: true });
+
+      // call the HomePage via the Router
+      Redirect("/");
+      */
+    } catch (error) {
+      console.error("LoginPage::error: ", error);
+    }
+  }
 }
+
+
 
 export default LoginPage;
