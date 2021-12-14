06e84c929330219bfcfa091f41224a66bc1bccd0
Alexandre <alexandre.denys@student.vinci.be>
2021-12-14 16:35:54 +0100

    Add SceneBeforeGame

---
+++ src/Components/Game/SceneBeforeGame.js
@@ -0,0 +1,29 @@
+import Phaser from 'phaser';
+import eventsCenter from './EventCenter';
+
+
+class SceneBeforeGame extends Phaser.Scene
+{
+
+
+	constructor()
+	{
+		super('game-scene');
+		this.button = undefined;
+        this.background = undefined;
+	}
+
+    preload(){
+        this.load.atlas('button', 'assets/buttons/button_texture_atlas.png', 'assets/buttons/button_texture_atlas.json');
+    }
+
+    
+	create()
+	{
+		this.background = game.add.tileSprite(0, 0, 800, 600, 'background');
+        this.button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick, this, 'over', 'out', 'down');
+	}
+
+}
+
+export default SceneBeforeGame;
