8beee4e10b520cee8836c4d6cbd9133248cabf17
Zoé Levaux <zoe.levaux@student.vinci.be>
2021-12-18 19:41:23 +0100

    new scoreboard

--- src/Components/Pages/ScoreboardPage.js
+++ src/Components/Pages/ScoreboardPage.js
@@ -1,5 +1,7 @@
 import HomePage from "./HomePage";
 import { Redirect } from "../Router/Router";
+//import UIScoreSingleScore from "../Game/UIScoreSingleScore";
+import { getSessionObject } from "../../utils/session";
 
 function ScoreboardPage() {
     const pageDiv = document.querySelector("#page");
@@ -8,9 +10,13 @@ function ScoreboardPage() {
                                 <div class="col" id="col1"> <h6 class="display-6 m-4"> Single Player </h6> </div>
                                 <div class="col" id="col2"> <h6 class="display-6 m-4"> Two Players </h6> </div>
                             </div>
+                            <div class="row">
+                                <div class="col" id="col3"> <h6 class="display-6 m-4"> My scores </h6> </div>
+                            </div>
                         </div>`;
     ScoreboardSingleTable();
     ScoreboardCoopTable();
+    UserScore();
     // create a login form
     const submit = document.createElement("input");
     submit.value = "Go back to HomePage";
@@ -110,9 +116,10 @@ function ScoreboardPage() {
     header1.scope="col";
     const header2 = document.createElement("th");
     header2.innerText = "Player 1";
+    header2.scope="col";
     const header3 = document.createElement("th");
     header3.innerText = "Player 2";
-    header1.scope="col";
+    header3.scope="col";
     header.appendChild(header1);
     header.appendChild(header2);
     header.appendChild(header3);
@@ -141,5 +148,51 @@ function ScoreboardPage() {
       console.error("ScoreBoardSinglePlayerpage::error: ", err);
     });
 };
+
+function UserScore() {
+  const user = getSessionObject("user1");
+  const col = document.querySelector("#col3");
+  console.log("user : ", user);
+  
+    // create a wrapper to provide a responsive table
+    const tableWrapper = document.createElement("div");
+    tableWrapper.className = "table-responsive pt-5";
+    // create an HTMLTableElement dynamically, based on the scores data (Array of Objects)
+    const table = document.createElement("table");
+    table.className = "table";
+    tableWrapper.appendChild(table);
+    // deal with header
+    const thead = document.createElement("thead");
+    const header = document.createElement("tr");
+    thead.appendChild(header);
+    const header1 = document.createElement("th");
+    header1.innerText = "Best Score Single";
+    header1.scope="col";
+    const header2 = document.createElement("th");
+    header2.innerText = "Best Score Coop";
+    header2.scope="col";
+
+    header.appendChild(header1);
+    header.appendChild(header2);
+    table.appendChild(thead);
+
+    // deal with data rows for tbody
+    const tbody = document.createElement("tbody");
+
+    const line = document.createElement("tr");
+    line.scope="row";
+    const singleScoreCell = document.createElement("td");
+    singleScoreCell.innerText = user.bestScoreSingle;
+    line.appendChild(singleScoreCell);
+    const coopScoreCell = document.createElement("td");
+    coopScoreCell.innerText = user.bestScoreCoop;
+    line.appendChild(coopScoreCell);
+    tbody.appendChild(line);
+
+    table.appendChild(tbody);
+      // add the HTMLTableElement to the main, within the #page div
+      col.appendChild(tableWrapper);
+    
+}
 
 export default ScoreboardPage;
