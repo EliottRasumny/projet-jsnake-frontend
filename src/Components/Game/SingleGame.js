9c37f5e930fbfd13ae793d28a81dd31efe24eb24
Alexandre <alexandre.denys@student.vinci.be>
2021-12-18 17:13:19 +0100

    Adding missing controls

--- src/Components/Game/SingleGame.js
+++ src/Components/Game/SingleGame.js
@@ -64,6 +64,7 @@ class SingleGame extends Phaser.Scene
     //UIScene for scores
     this.scene.run('ui-score', 'Player');
     //Enabling keyboard inputs
+    this.controls = this.input.keyboard.createCursorKeys();
   };
 
 
